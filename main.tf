43417067ca2176f9a36aa7aafb6d6596536f9804
Konda <gosalakonda2000@gmail.com>
2025-08-07 14:16:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,10 +24,10 @@ resource "aws_subnet" "eks_subnet_a" {
 resource "aws_subnet" "eks_subnet_b" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-2a"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
   tags = {
-    Name = "2048-game-subnet-a"
+    Name = "2048-game-subnet-b"
   }
 }
 
