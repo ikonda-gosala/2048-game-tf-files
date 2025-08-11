5e2e52ef14a8e029908379e98a94e0df05103930
Konda <gosalakonda2000@gmail.com>
2025-08-11 10:46:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,120 +1,141 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
+# 1. vpc
 resource "aws_vpc" "eks_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
+  cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
+  enable_dns_support = true
   tags = {
-    Name = "2048-game"
+    Name = "eks_vpc"
   }
 }
 
-resource "aws_subnet" "eks_subnet_a" {
-  vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
+# 2. Subnets
+resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.eks_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_customer_owned_ip_on_launch = true
   tags = {
-    Name = "2048-game-subnet-a"
+    Name = "sunet1"
   }
 }
 
-resource "aws_subnet" "eks_subnet_b" {
-  vpc_id                  = aws_vpc.eks_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
+resource "aws_subnet" "subnet2" {
+  vpc_id = aws_vpc.eks_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_customer_owned_ip_on_launch = true
   tags = {
-    Name = "2048-game-subnet-b"
+    Name = "subnet2"
   }
 }
 
+# 3.Internet gateway + Route table
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.eks_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+resource "aws_route_table_association" "a2" {
+  subnet_id = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# 4.EkS Cluster IAM role
+data "aws_iam_policy_document" "eks_assume_role" {
+  statement{
+    actions = ["sts:AssumeRole"]
+    principals{
+      type = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
 resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-    }]
-  })
+  name = "eksClusterRole"
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
 }
-
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
+  role = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_eks_cluster" "eks_cluster" {
-  name     = "my-eks-cluster"
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = [
-      aws_subnet.eks_subnet_a.id,
-      aws_subnet.eks_subnet_b.id
-    ]
+# 5.Security Group
+resource "aws_security_group" "eks_sg" {
+  name = "eks-cluster-sg"
+  vpc_id = aws_vpc.eks_vpc.id
+  description = "Eks cluster security group."
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
+# 6.Eks cluster
+resource "aws_eks_cluster" "my_cluster" {
+  name = "my_cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  vpc_config {
+    subnet_ids = [aws_subnet.subnet1.id,aws_subnet.subnet2.id]
+    security_group_ids = [aws_security_group.eks_sg.id]
+  }
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
 }
 
-resource "aws_iam_role" "eks_node_role" {
-  name = "eks-node-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-  })
+# 7.Node group IAM role
+data "aws_iam_policy_document" "node_assume_role" {
+  statement{
+    actions = ["sts:AssumeRole"]
+    principals{
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
 }
-
-resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
-  role       = aws_iam_role.eks_node_role.name
+resource "aws_iam_role" "eks_node_role" {
+  name = "eksnoderole"
+  assume_role_policy = data.aws_iam_policy_document.node_assume_role.json
+}
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
+  role = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
-
-resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-  role       = aws_iam_role.eks_node_role.name
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
+  role = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+resource "aws_iam_role_policy_attachment" "node_CNI" {
+  role = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "ec2_container_registry_read_only" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
+# Node group
 resource "aws_eks_node_group" "eks_nodes" {
-  cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "eks-node-group"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = [
-    aws_subnet.eks_subnet_a.id,
-    aws_subnet.eks_subnet_b.id
-  ]
-
+  cluster_name = aws_eks_cluster.my_cluster.name
+  node_group_name = "my-node-group"
+  node_role_arn = aws_iam_role.eks_node_role.arn
+  subnet_ids = [aws_subnet.subnet1.id,aws_subnet.subnet2.id]
   scaling_config {
     desired_size = 2
-    max_size     = 2
-    min_size     = 1
+    max_size = 2
+    min_size = 1
   }
-
   instance_types = ["t3.micro"]
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_worker_node_policy,
-    aws_iam_role_policy_attachment.eks_cni_policy,
-    aws_iam_role_policy_attachment.ec2_container_registry_read_only
+  depends_on = [ 
+  aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
+  aws_iam_role_policy_attachment.node_CNI,
+  aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly 
   ]
 }
